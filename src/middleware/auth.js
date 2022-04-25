9fc17a815631b7b5df9a70f41ffc4a31a3df862c
“Ridhi1316” <ridhi9697@gmail.com>
2022-04-26 00:16:58 +0530

    April-21/MiddleWare,headers,token,autheticate,authorise to change user login posts

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,44 @@
-const authenticate = function(req, req, next) {
+const jwt = require("jsonwebtoken");
+
+const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
-
-    next()
+    console.log(req.headers)
+    try {
+      console.log(req.headers);
+        token = req.headers["x-auth-token"];
+        console.log(token);
+      if (!token) {
+        return res.send({ status: false, msg: "Token must be present" });
+      }
+  
+      let verifyUser = jwt.verify(token, "FunctionUp-Uranium");
+  
+      if (!verifyUser) {
+        return res.send({ status: false, msg: "Token is incorrect" });
+      }
+      next();
+    } catch (err) {
+      res.status(400).send({ status:"false",msg: err })
+    }
+    
 }
-
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
-    next()
+    try {
+        let token = req.headers["x-auth-token"];
+        let decodedToken = jwt.verify(token, "FunctionUp-Uranium")
+        let userToBeModified = req.params.userId
+        let userLoggedIn = decodedToken.userId
+        if (userToBeModified != userLoggedIn) {
+          return res.send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
+        } else {
+          next();
+        }
+      } catch (err) {
+        res.status(400).send({ msg: err })
+      }
 }
+module.exports.authenticate = authenticate;
+module.exports.authorise =authorise;
